093c87138bef65c8e4c8bdfdd8af5fa13ac0cc6b
zhanngxiao <304899668@qq.com>
2017-10-25 20:36:25 +0800

    10.25作业 张晓

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+/* GET home page. */
+router.post('/zx', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin','*')
+    var temp=[]
+    if(req.body.abc==1){
+      temp=['1','2','3']
+    }else {
+        temp=['4','5','6']
+    }
+   res.send(temp)
+});
+
+module.exports = router;
